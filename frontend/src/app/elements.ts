f68bf4f287ae52bb41e1368edd5b618c5b289fe7
Isabelle-WI <schandai.wwi21@student.dhbw-heidenheim.de>
2023-03-20 15:27:24 +0100

    Frontend Vorbereitung

---
+++ frontend/src/app/elements.ts
@@ -0,0 +1,5 @@
+
+
+export interface Elements {
+    title: string;
+}
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,10 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { FormBuilder } from '@angular/forms';
+import { Elements } from '../elements';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+
+
+
+  constructor(private http: HttpClient, private formbuilder: FormBuilder){
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  post(): void {
+        //Backend-Pfad
+        const url = 'http://localhost:8080/new-element';
+
+        //In JSON Format
+        const headers = new HttpHeaders().set('Accept', 'application/json');
+    
+        //Bearbeitung als Post-Request
+        this.http.post<Elements>(url/*, newElement*/, { headers }).subscribe({
+          next: (element:any) => {
+            console.log('Neuer Titel: ', element.shortHand);
+            console.log(element);
+          },
+          error: (err) => {
+            console.error('Error', err);
+          }
+        });
+  }
+
+  get(): void {
+          //Backend-Pfad
+          const url = 'http://localhost:8080/home';
+
+          //Antwort im Json Format
+          const headers = new HttpHeaders().set('Accept', 'application/json');
+    
+          this.http.get<Elements[]>(url, { headers }).subscribe({
+            next: (element:any) => {
+              console.log(element);
+            },
+            error: (err:any) => {
+              console.error('Error', err);
+            }
+          });
+  }
+
+  put(): void {
+    const url = 'http://localhost:8080/home';
+
+    const headers = new HttpHeaders().set('Accept', 'application/json');
+
+    this.http.put<Elements>(url, { headers }).subscribe({
+      next: (element:any) => {
+        console.log(element);
+      },
+      error: (err) => {
+          console.error('Error', err);
+      }
+    })
+  }
+
+  delete(): void {
+          //Backend-Pfad
+          const url = 'http://localhost:8080/home'; //Pfad anpassen
+
+          //Antwort im json Format
+          const headers = new HttpHeaders().set('Accept', 'application/json');
+    
+          this.http.delete<Elements>(url, { headers }).subscribe({
+            next: (element:any) => {
+              console.log(element);
+            },
+            error: (err:any) => {
+              console.error('Error', err);
+            }
+          });
+  }
+
+
+
+
+
 
 }
